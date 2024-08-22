4ccc92cd368ebdfa9b8ba30fa9ecf416cf57b878
briveros23 <briveros@unal.edu.co>
2024-08-21 19:57:50 -0500

    se añadieron los datos y se guardaron los grafos para el analisis

---
+++ notebook/consolidar bases de datos Twitter reformatributaria.R
@@ -0,0 +1,23 @@
+# crear crear grafos bases de datos samuel(twitter)
+library(dplyr)
+library(ggplot2)
+library(igraph)
+
+rm(list = ls())
+setwd("C:/Users/bcriv/OneDrive/Escritorio/DNI redes sociales/DNI_redes_sociales/notebook")
+
+
+# Read Data ----
+df_raw <- read.csv("../input/reforma_tributaria_04_11_2022_5pm_mixed_spanish.csv")
+
+df_raw %>% tibble %>% filter(tweet_type == "retweet") %>% 
+  select(from_user_id,to_user_id,
+         from_user_screen_name,to_user_screen_name,
+         from_user_botscore,to_user_botscore) -> df
+
+G <- graph_from_data_frame(select(df,from_user_id,to_user_id),directed = T)
+G <- simplify(G)
+
+save(G,df,file = "../input/out_refTrib.RData")
+
+
